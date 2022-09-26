4a2fb79720ee835c4dad55d2319a2d9e0c478471
Almudena <almudenapalaciosibanez@gmail.com>
2022-09-26 16:18:50 +0200

    Reto-2 solved

---
+++ Reto-2/Program.cs
@@ -0,0 +1,17 @@
+﻿//Fibonacci
+
+int[] fibonacci = new int[50];
+
+fibonacci[0] = 0;
+fibonacci[1] = 1;
+
+for (int i = 2; i < fibonacci.Length; i++)
+{
+    fibonacci[i] = fibonacci[i - 1] + fibonacci[i - 2];
+    fibonacci.Append(fibonacci[i]);
+}
+
+foreach (int i in fibonacci)
+{
+    Console.Write(i + ", ");
+}
