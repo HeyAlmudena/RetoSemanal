c934abbefd459ed0e0a93f9719ca7f4278b85214
Almudena <almudenapalaciosibanez@gmail.com>
2022-09-27 19:17:40 +0200

    Reto-7

--- Reto-7/Program.cs
+++ Reto-7/Program.cs
@@ -1,2 +1,8 @@
-﻿// See https://aka.ms/new-console-template for more information
-Console.WriteLine("Hello, World!");
+﻿/*
+ * Crea un programa que cuente cuantas veces se repite cada palabra
+ * y que muestre el recuento final de todas ellas.
+ * - Los signos de puntuación no forman parte de la palabra.
+ * - Una palabra es la misma aunque aparezca en mayúsculas y minúsculas.
+ * - No se pueden utilizar funciones propias del lenguaje que
+ *   lo resuelvan automáticamente.
+ */
