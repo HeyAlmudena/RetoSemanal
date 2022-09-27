b4e2976528293a397f47437c342c6c50c6daeca0
Almudena <almudenapalaciosibanez@gmail.com>
2022-09-27 17:36:11 +0200

    Reto 5 solved

---
+++ Reto-5/Program.cs
@@ -0,0 +1,17 @@
+﻿/*
+ * Crea un programa que invierta el orden de una cadena de texto
+ * sin usar funciones propias del lenguaje que lo hagan de forma automática.
+ * - Si le pasamos "Hola mundo" nos retornaría "odnum aloH"
+ */
+
+Console.WriteLine("Escribe una frase: ");
+string cadena = Console.ReadLine();
+
+char[] cadenaDescompuesta = cadena.ToCharArray();
+char[] cadenaInversa = new char[cadenaDescompuesta.Length];
+
+for (int i = 0; i < cadenaDescompuesta.Length; i++)
+{
+    cadenaInversa[i] = cadenaDescompuesta[cadenaDescompuesta.Length - i - 1];
+    Console.Write(cadenaInversa[i]);
+}
