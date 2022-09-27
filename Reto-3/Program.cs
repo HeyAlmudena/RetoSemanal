2580a3af947c221b6af26eeeda43336528931a4c
Almudena <almudenapalaciosibanez@gmail.com>
2022-09-27 17:11:43 +0200

    Reto-3 changed

--- Reto-3/Program.cs
+++ Reto-3/Program.cs
@@ -3,46 +3,33 @@
  * Hecho esto, imprime los números primos entre 1 y 100.
  */
 
-Console.WriteLine("Introduce un número: ");
-int numero = int.Parse(Console.ReadLine());
+int[] numerosPrimos = new int[] { };
 
-int[] numerosPrimos = new int[99];
-
-ComprobacionNumero(numero);
-NumerosPrimos();
-
-// Comprobación del número primo
-void ComprobacionNumero(int numero)
+for (int i = 1; i < 101; i++)
 {
-    bool isPrimo;
-
-    if (numero == 2 || numero == 3 || numero == 5 || numero == 7)
+    if (NumeroPrimo(i))
     {
-        isPrimo = true;
-    }
-
-    else if (numero % 2 == 0 || numero % 3 == 0 || numero % 5 == 0 || numero % 7 == 0)
-    {
-        isPrimo = false;
-    }
-
-    else
-    {
-        isPrimo = true;
-    }
-
-    if (isPrimo)
-    {
-        numerosPrimos.Append(numero);
-        Console.WriteLine(numero + ", ");
-    }
+        numerosPrimos.Append(i);
+        Console.WriteLine(i);
+    }    
 }
 
-// Números primos entre 1 y 100
-void NumerosPrimos()
-{
-    for (int i = 1; i< numerosPrimos.Length; i++)
+NumeroPrimo(8);
+
+bool NumeroPrimo(int numero)
+{  
+    if (numero <= 1)
     {
-        
+        return false;
     }
+
+    for (int i = 2; i < numero; i++)
+    {
+        if (numero%i == 0)
+        {
+            return false;
+        }
+    }
+
+    return true;
 }
